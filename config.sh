33544649bf217f8a45fe7255895d10c5f9325d94
Denis Scheglov <dscheglov@ptsecurity.com>
2018-04-27 20:43:31 +0300

    allow to use $PGDATA env variable

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-sed -ri "s/#log_statement = 'none'/log_statement = 'all'/g" /var/lib/postgresql/data/postgresql.conf
+sed -ri "s/#log_statement = 'none'/log_statement = 'all'/g" ${PGDATA:-/var/lib/postgresql/data}/postgresql.conf
